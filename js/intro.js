9f99d6e414f1ab2a64355bb2ee408c8af1b64a12
yw.choi <query3mymy@gmail.com>
2022-09-25 17:48:22 +0900

    20220925 -> 주석 수정(* modify 수정 안함)

--- js/intro.js
+++ js/intro.js
@@ -2,6 +2,7 @@
  * 로그인 화면
  * pre - 2022.09.09
  * modify - 2022.09.24
+ * milestone - 2022.12.30
  * dev : 이규호, 최양원
  * copyright : 이규호, 최양원
  */
@@ -77,6 +78,7 @@ $(document).ready(function () {
     });
 });
 
+// 아이디 저장 query
 function setCookie(cookieName, value, exdays) {
     var exdate = new Date();
     exdate.setDate(exdate.getDate() + exdays);
